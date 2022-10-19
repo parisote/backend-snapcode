1be8b5e7db572dab9f4e4c47b8fcaca76472f98d
parisote <detomas@gmail.com>
2022-10-19 14:25:44 -0300

    fix: pifie el user je

--- test/auth.js
+++ test/auth.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios");
 
 describe("Auth test",() => {
   it("Register Test OK", async () => {
-      const body = { email: "testeo@test.com", password: "123" }
+      const body = { email: "test@test.com", password: "123" }
 
       const path = 'http://localhost:3000/api/auth/register'
       const result = await axios.post(path, body);
