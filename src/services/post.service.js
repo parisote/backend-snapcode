c247f2b3cd2f2c47272cad99a6fec0d171166dfb
harushishi <juanmaac321123@gmail.com>
2022-10-23 19:49:47 -0300

    fixes

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -76,7 +76,7 @@ class PostService {
 
         return result
     }*/
-    
+
 
     async getById(postId) {
         const result = await this.prisma.post.findUniqueOrThrow({
@@ -84,17 +84,17 @@ class PostService {
                 id: Number(postId)
             }, include: {
                 code: true,
-                commentaries:{
-                    select:{
-                       id:true,
-                       text:true,
-                       imgUrl: true,
-                       postId: true,
-                       authorId: true,
-                       likedBy:{ select:{ id:true }}   
+                commentaries: {
+                    select: {
+                        id: true,
+                        text: true,
+                        imgUrl: true,
+                        postId: true,
+                        authorId: true,
+                        likedBy: { select: { id: true } }
                     }
                 },
-                likedBy: {select:{id:true}}
+                likedBy: { select: { id: true } }
             }
         })
         return result
@@ -106,8 +106,17 @@ class PostService {
                 authorId: Number(userId)
             }, include: {
                 code: true,
-                likedBy: true,
-                commentaries: true,
+                commentaries: {
+                    select: {
+                        id: true,
+                        text: true,
+                        imgUrl: true,
+                        postId: true,
+                        authorId: true,
+                        likedBy: { select: { id: true } }
+                    }
+                },
+                likedBy: { select: { id: true } }
             }
         })
         return result
